b7f1fea6d581de5578b4e9e2e1a332278b66076f
sarafay2003 <sarafay2003@gmail.com>
2026-09-25 16:24:31 +0400

    YOLOv8 training complete (mAP50 0.74), verified on real validation image

--- test_trained_model.py
+++ test_trained_model.py
@@ -1,5 +1,17 @@
+# from ultralytics import YOLO
+#
+# model = YOLO('models/best.pt')
+# results = model('bus.jpg')
+# results[0].show()
+
+#from ultralytics import YOLO
+#
+# model = YOLO('models/best.pt')
+# results = model('Car_damage_detection-3/valid/images/SOME_IMAGE_NAME.jpg')
+# results[0].show()
+
 from ultralytics import YOLO
 
 model = YOLO('models/best.pt')
-results = model('bus.jpg')
+results = model('Car_damage_detection-3/valid/images/003305_jpg.rf.6aadc1f0ab75b72eac663254c1a4630c.jpg')
 results[0].show()
